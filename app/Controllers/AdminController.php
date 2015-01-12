0dabe3f3cd8d6c0bf3c7b66140669e07469f6d1a
Awalin Yudhana <awalin.yudhana@nanomit.es>
2015-01-12 19:48:14 +0700

    login intregartion 01

--- app/Controllers/AdminController.php
+++ app/Controllers/AdminController.php
@@ -3,31 +3,38 @@
 
 namespace Mabes\Controllers;
 
+use Mabes\Core\SecurityMiddleware;
+
 class AdminController extends BaseController
 {
     public function index()
     {
-//        $this->app->render('Pages/_admin.twig');
+        $this->app->render('Pages/_admin.twig');
     }
 
     public function login()
     {
-        if ($this->app->request->isGet()) {
+        if ($this->app->session->token === true) {
+            $this->app->redirect('/administrator');
         }
+        $data = array();
         if ($this->app->request->isPost()) {
             $username = $this->app->request->params('username');
             $password = $this->app->request->params('password');
 
-
             $staff = $this->app->em->getRepository("Mabes\\Entity\\Staff")->findOneBy(["username" => "$username"]);
             if ($staff != null) {
+                print_r($staff);
                 if ($staff->verifyPassword($password)) {
-                    echo "yes";
+                    $this->app->session->token = true;
+                    $this->app->redirect('/administrator');
                 }
             }
-            $this->app->flashNow('error', "invalid username and password");
+            $data['isError'] = true;
+            $data['errorTitle'] = "Login Message";
+            $data['errorMessage'] = "invalid username and password";
         }
-        $this->app->render('Pages/_login.twig');
+        $this->app->render('Pages/_login.twig', $data);
     }
 
 }
