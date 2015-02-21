dc8d9109ac8db1cdfffa31f051d9bfe9cf4c624e
Prasetyo Wicaksono <jowy@oiry.net>
2015-02-21 16:16:50 +0700

    oops AuthCommand forgot to commit

---
+++ app/Service/Command/AuthCommand.php
@@ -0,0 +1,72 @@
+<?php
+
+
+namespace Mabes\Service\Command;
+
+use Mabes\Core\CommonBehaviour\MassAssignmentTrait;
+use Mabes\Core\CommonBehaviour\RepositoryAwareTrait;
+use Mabes\Core\Contracts\CommandInterface;
+use Mabes\Entity\Staff;
+use Symfony\Component\Validator\Constraints as Assert;
+
+class AuthCommand implements CommandInterface
+{
+    use RepositoryAwareTrait;
+    use MassAssignmentTrait;
+
+    /**
+     * @Assert\NotBlank(message="Username tidak boleh kosong")
+     * @var string
+     */
+    private $username;
+
+    /**
+     * @Assert\NotBlank(message="Password tidak boleh kosong")
+     * @var string
+     */
+    private $password;
+
+    /**
+     * @return string
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * @param string $username
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param string $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * @Assert\False(message="Staff tidak ditemukan didalam database")
+     */
+    public function isStaffExist()
+    {
+        $staff = $this->getRepository()->findOneBy(["username" => $this->getUsername()]);
+
+        return $staff instanceof Staff ? false : true;
+    }
+}
+
+// EOF
